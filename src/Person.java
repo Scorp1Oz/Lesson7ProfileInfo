139b4354095915456b0e57c2cbb9a250506f93a3
Scorp1Oz <111981572+Scorp1Oz@users.noreply.github.com>
2023-10-26 20:43:08 +0300

    Update

---
+++ src/Person.java
@@ -0,0 +1,6 @@
+public class Person {
+
+    public static String personInfo(String firstName, String lastName, String city, String phoneNumber) {
+        return "Зателефонувати громадянинові " + firstName + " " + lastName + " з міста " + city + " можна за номером " + phoneNumber + ".";
+    }
+}
---
+++ src/Project.java
@@ -0,0 +1,13 @@
+public class Project {
+
+    public static void main(String[] args) {
+        String info1 = Person.personInfo("Will", "Smith", "New York", "2936729462846");
+        String info2 = Person.personInfo("Jackie", "Chan", "Shanghai", "12312412412");
+        String info3 = Person.personInfo("Sherlock", "Holmes", "London", "37742123513В");
+
+        System.out.println(info1);
+        System.out.println(info2);
+        System.out.println(info3);
+    }
+}
+
